69438a97b4767579d074cd4999c1ba8e4437a3ac
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-03-19 16:05:50 +0300

    Добавьте файлы проекта.

---
+++ lesson17/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class BankAccount<T>
+    {
+        T num;
+        double balance;
+        string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Balance
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string GetInfo()
+        {
+            return $"{num} {name} {balance}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            this.num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+            Console.WriteLine("Введите имя");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            this.balance = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+
+}
---
+++ lesson17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> person1 = new BankAccount<int> ();
+
+            BankAccount<string> person2 = new BankAccount<string>();
+            person1.Input();
+            person2.Input();
+
+            Console.WriteLine($"Аккаунт1: {person1.GetInfo()}");
+            Console.WriteLine($"Аккаунт2: {person2.GetInfo()}");
+            Console.ReadKey();
+        }
+    }
+}
